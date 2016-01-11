155e9b511d6da7887d9a85cc7e688bc995af1a94
BeFra <ben.frank@fau.de>
2016-01-11 16:57:02 +0100

    init semaphore commit

---
+++ 2_Exercise/lwt/mutex/lwt-sem.cc
@@ -0,0 +1,38 @@
+
+
+/// @file lwt-sem.cc
+/// @brief Implementation for semaphore functions
+
+
+struct lwt_sem;
+
+/// @brief Creates a new lwt_sem
+///
+/// @param num Starting value of internal counter.
+///
+/// @return New semaphore, @c 0 on failure
+struct lwt_sem* lwt_sem_init(unsigned int num);
+
+/// @brief Destroy a semaphore.
+///
+/// @param sem Semaphore to be destroyed.
+void lwt_sem_destroy(struct lwt_sem *sem);
+
+/// @brief Signal a semaphore
+///
+/// Signal a semaphore, if threads are waiting on this semaphore (and the
+/// internal counter is 0) a single thread is woken up. If no thread waits on
+/// this semaphore, then the internal counter is incremented.
+///
+/// @param sem Semaphore to be signaled.
+void lwt_sem_signal(struct lwt_sem *sem);
+
+/// @brief Wait on a semaphore
+///
+/// if the internal counter is > 0 it is decremented and the calling thread
+/// returns, else it waits on the semaphore until it is signaled.
+///
+/// @param sem Semaphore to wait on.
+void lwt_sem_wait(struct lwt_sem *sem);
+
+
